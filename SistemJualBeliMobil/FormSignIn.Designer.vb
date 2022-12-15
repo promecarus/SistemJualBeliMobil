2591c9a86bd6176921859521d11a11a7dbed706e
Dynavx <haical31@gmail.com>
2022-12-15 15:18:16 +0700

    Update form login
    bismillah

--- SistemJualBeliMobil/FormSignIn.Designer.vb
+++ SistemJualBeliMobil/FormSignIn.Designer.vb
@@ -22,17 +22,34 @@ Partial Class FormSignIn
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = Global.SistemJualBeliMobil.My.Resources.Resources.BannerSign
+        Me.PictureBox1.Location = New System.Drawing.Point(-2, -2)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(416, 576)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
         '
         'FormSignIn
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 20.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.ClientSize = New System.Drawing.Size(819, 575)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
         Me.Name = "FormSignIn"
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "Sign In"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
 
     End Sub
+
+    Friend WithEvents PictureBox1 As PictureBox
 End Class
--- SistemJualBeliMobil/FormSignIn.vb
+++ SistemJualBeliMobil/FormSignIn.vb
@@ -2,4 +2,8 @@
     Private Sub FormSignInAdmin_Closed(sender As Object, e As EventArgs) Handles Me.Closed
         Form1.Show()
     End Sub
+
+    Private Sub FormSignIn_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
 End Class
