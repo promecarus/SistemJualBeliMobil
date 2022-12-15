1b9ebcd3f763fd4c9099477e3156391e1d8b0d6a
promecarus <haikalslipi@gmail.com>
2022-12-15 11:57:23 +0700

    Update form 1

--- SistemJualBeliMobil/Form1.Designer.vb
+++ SistemJualBeliMobil/Form1.Designer.vb
@@ -22,10 +22,66 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.ButtonFormSignInAdmin = New System.Windows.Forms.Button()
+        Me.ButtonFormSignUpAdmin = New System.Windows.Forms.Button()
+        Me.ButtonFormSignInUser = New System.Windows.Forms.Button()
+        Me.ButtonFormSignUpUser = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'ButtonFormSignInAdmin
+        '
+        Me.ButtonFormSignInAdmin.Location = New System.Drawing.Point(12, 12)
+        Me.ButtonFormSignInAdmin.Name = "ButtonFormSignInAdmin"
+        Me.ButtonFormSignInAdmin.Size = New System.Drawing.Size(200, 29)
+        Me.ButtonFormSignInAdmin.TabIndex = 0
+        Me.ButtonFormSignInAdmin.Text = "Form Sign In Admin"
+        Me.ButtonFormSignInAdmin.UseVisualStyleBackColor = True
+        '
+        'ButtonFormSignUpAdmin
+        '
+        Me.ButtonFormSignUpAdmin.Location = New System.Drawing.Point(218, 12)
+        Me.ButtonFormSignUpAdmin.Name = "ButtonFormSignUpAdmin"
+        Me.ButtonFormSignUpAdmin.Size = New System.Drawing.Size(200, 29)
+        Me.ButtonFormSignUpAdmin.TabIndex = 0
+        Me.ButtonFormSignUpAdmin.Text = "Form Sign Up Admin"
+        Me.ButtonFormSignUpAdmin.UseVisualStyleBackColor = True
+        '
+        'ButtonFormSignInUser
+        '
+        Me.ButtonFormSignInUser.Location = New System.Drawing.Point(12, 47)
+        Me.ButtonFormSignInUser.Name = "ButtonFormSignInUser"
+        Me.ButtonFormSignInUser.Size = New System.Drawing.Size(200, 29)
+        Me.ButtonFormSignInUser.TabIndex = 0
+        Me.ButtonFormSignInUser.Text = "Form Sign In User"
+        Me.ButtonFormSignInUser.UseVisualStyleBackColor = True
+        '
+        'ButtonFormSignUpUser
+        '
+        Me.ButtonFormSignUpUser.Location = New System.Drawing.Point(218, 47)
+        Me.ButtonFormSignUpUser.Name = "ButtonFormSignUpUser"
+        Me.ButtonFormSignUpUser.Size = New System.Drawing.Size(200, 29)
+        Me.ButtonFormSignUpUser.TabIndex = 0
+        Me.ButtonFormSignUpUser.Text = "Form Sign Up User"
+        Me.ButtonFormSignUpUser.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 20.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.ButtonFormSignUpUser)
+        Me.Controls.Add(Me.ButtonFormSignUpAdmin)
+        Me.Controls.Add(Me.ButtonFormSignInUser)
+        Me.Controls.Add(Me.ButtonFormSignInAdmin)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
     End Sub
 
+    Friend WithEvents ButtonFormSignInAdmin As Button
+    Friend WithEvents ButtonFormSignUpAdmin As Button
+    Friend WithEvents ButtonFormSignInUser As Button
+    Friend WithEvents ButtonFormSignUpUser As Button
 End Class
--- SistemJualBeliMobil/Form1.vb
+++ SistemJualBeliMobil/Form1.vb
@@ -1,3 +1,21 @@
 ﻿Public Class Form1
+    Private Sub ButtonFormSignInAdmin_Click(sender As Object, e As EventArgs) Handles ButtonFormSignInAdmin.Click
+        FormSignInAdmin.Show()
+        Me.Hide()
+    End Sub
 
+    Private Sub ButtonFormSignUpAdmin_Click(sender As Object, e As EventArgs) Handles ButtonFormSignUpAdmin.Click
+        FormSignUpAdmin.Show()
+        Me.Hide()
+    End Sub
+
+    Private Sub ButtonFormSignInUser_Click(sender As Object, e As EventArgs) Handles ButtonFormSignInUser.Click
+        FormSignInUser.Show()
+        Me.Hide()
+    End Sub
+
+    Private Sub ButtonFormSignUpUser_Click(sender As Object, e As EventArgs) Handles ButtonFormSignUpUser.Click
+        FormSignUpUser.Show()
+        Me.Hide()
+    End Sub
 End Class
