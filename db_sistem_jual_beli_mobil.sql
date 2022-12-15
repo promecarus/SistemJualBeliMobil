a00c7ca65fac3c6794aa7dbba16565a2d4181723
promecarus <haikalslipi@gmail.com>
2022-12-15 10:07:24 +0700

    Add ddl users

--- db_sistem_jual_beli_mobil.sql
+++ db_sistem_jual_beli_mobil.sql
@@ -4,3 +4,10 @@ CREATE DATABASE db_sistem_jual_beli_mobil;
 
 USE db_sistem_jual_beli_mobil;
 
+CREATE TABLE users(
+	id_user INT(11) PRIMARY KEY AUTO_INCREMENT,
+	username VARCHAR(30) UNIQUE,
+	email VARCHAR(100) UNIQUE,
+	password VARCHAR(50)
+);
+
