936040b12aa53020f6ce36310b06d609fd18e456
Dynavx <haical31@gmail.com>
2022-12-15 15:50:49 +0700

    update form sign in

--- SistemJualBeliMobil/FormSignIn.Designer.vb
+++ SistemJualBeliMobil/FormSignIn.Designer.vb
@@ -23,6 +23,14 @@ Partial Class FormSignIn
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.inputUsername = New System.Windows.Forms.TextBox()
+        Me.SigninBtn = New System.Windows.Forms.Button()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
@@ -31,16 +39,107 @@ Partial Class FormSignIn
         Me.PictureBox1.Image = Global.SistemJualBeliMobil.My.Resources.Resources.BannerSign
         Me.PictureBox1.Location = New System.Drawing.Point(-2, -2)
         Me.PictureBox1.Name = "PictureBox1"
-        Me.PictureBox1.Size = New System.Drawing.Size(416, 576)
+        Me.PictureBox1.Size = New System.Drawing.Size(409, 507)
         Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
         Me.PictureBox1.TabIndex = 0
         Me.PictureBox1.TabStop = False
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Segoe UI", 20.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Label1.Location = New System.Drawing.Point(454, 40)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(104, 37)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Sign in"
+        '
+        'inputUsername
+        '
+        Me.inputUsername.Font = New System.Drawing.Font("Segoe UI", 15.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.inputUsername.Location = New System.Drawing.Point(459, 153)
+        Me.inputUsername.Name = "inputUsername"
+        Me.inputUsername.Size = New System.Drawing.Size(322, 34)
+        Me.inputUsername.TabIndex = 2
+        '
+        'SigninBtn
+        '
+        Me.SigninBtn.BackColor = System.Drawing.Color.FromArgb(CType(CType(15, Byte), Integer), CType(CType(34, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.SigninBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.SigninBtn.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.SigninBtn.ForeColor = System.Drawing.Color.White
+        Me.SigninBtn.Location = New System.Drawing.Point(459, 292)
+        Me.SigninBtn.Name = "SigninBtn"
+        Me.SigninBtn.Size = New System.Drawing.Size(322, 37)
+        Me.SigninBtn.TabIndex = 3
+        Me.SigninBtn.Text = "Sign in"
+        Me.SigninBtn.UseVisualStyleBackColor = False
+        '
+        'Button1
+        '
+        Me.Button1.BackColor = System.Drawing.Color.White
+        Me.Button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button1.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Button1.ForeColor = System.Drawing.Color.FromArgb(CType(CType(15, Byte), Integer), CType(CType(34, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.Button1.Location = New System.Drawing.Point(459, 356)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(322, 37)
+        Me.Button1.TabIndex = 4
+        Me.Button1.Text = "Sign in"
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Label2.Location = New System.Drawing.Point(603, 332)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(26, 21)
+        Me.Label2.TabIndex = 5
+        Me.Label2.Text = "or"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Font = New System.Drawing.Font("Segoe UI", 15.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.TextBox1.Location = New System.Drawing.Point(459, 226)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(322, 34)
+        Me.TextBox1.TabIndex = 6
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Label3.Location = New System.Drawing.Point(454, 129)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(87, 21)
+        Me.Label3.TabIndex = 7
+        Me.Label3.Text = "Username"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Label4.Location = New System.Drawing.Point(454, 202)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(82, 21)
+        Me.Label4.TabIndex = 8
+        Me.Label4.Text = "Password"
         '
         'FormSignIn
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(819, 575)
+        Me.BackColor = System.Drawing.Color.White
+        Me.ClientSize = New System.Drawing.Size(824, 503)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.SigninBtn)
+        Me.Controls.Add(Me.inputUsername)
+        Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.PictureBox1)
         Me.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
         Me.Name = "FormSignIn"
@@ -48,8 +147,17 @@ Partial Class FormSignIn
         Me.Text = "Sign In"
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
 
     Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents inputUsername As TextBox
+    Friend WithEvents SigninBtn As Button
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label2 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
 End Class
--- SistemJualBeliMobil/FormSignIn.vb
+++ SistemJualBeliMobil/FormSignIn.vb
@@ -2,8 +2,4 @@
     Private Sub FormSignInAdmin_Closed(sender As Object, e As EventArgs) Handles Me.Closed
         Form1.Show()
     End Sub
-
-    Private Sub FormSignIn_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-
-    End Sub
 End Class
