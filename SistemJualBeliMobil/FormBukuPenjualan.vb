f50afd9f75869ef1fa5d3586742b42ce07725518
promecarus <haikalslipi@gmail.com>
2022-12-16 21:20:48 +0700

    Add form buku penjualan cell click

--- SistemJualBeliMobil/FormBukuPenjualan.vb
+++ SistemJualBeliMobil/FormBukuPenjualan.vb
@@ -15,6 +15,18 @@
         DataGridViewBukuPenjualan.ClearSelection()
     End Sub
 
+    Private Sub DataGridViewBukuPenjualan_CellClick(sender As Object, e As DataGridViewCellEventArgs) Handles DataGridViewBukuPenjualan.CellClick
+        Try
+            Dim index As Integer = e.RowIndex
+            Dim selectedRow As DataGridViewRow
+            selectedRow = DataGridViewBukuPenjualan.Rows(index)
+
+            selectedRowBukuPenjualan = selectedRow.Cells(0).Value
+        Catch ex As Exception
+            MessageBox.Show("Fitur sorting belum tersedia!")
+        End Try
+    End Sub
+
     Private Sub FormBukuPenjualan_Closed(sender As Object, e As EventArgs) Handles Me.Closed
         Form1.Show()
     End Sub
