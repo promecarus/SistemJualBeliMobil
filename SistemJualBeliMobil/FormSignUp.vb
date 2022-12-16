5bcb95a1906e482d49dfaf5d701b8e166e58f8a5
Sandhi Karunia Sugihartana <sandhi372@gmail.com>
2022-12-16 08:33:49 +0700

    BE Sign In / Sign Up

--- SistemJualBeliMobil/FormSignUp.vb
+++ SistemJualBeliMobil/FormSignUp.vb
@@ -1,5 +1,53 @@
 ﻿Public Class FormSignUp
-    Private Sub FormSignUpAdmin_Closed(sender As Object, e As EventArgs) Handles Me.Closed
-        Form1.Show()
+
+    Private Sub SignupBtn_Click(sender As Object, e As EventArgs) Handles SignupBtn.Click
+        If FormSignIn.Users.ValidateUser(inputUsername.Text) = True Then
+            MessageBox.Show("Username Sudah Terdaftar !!")
+        Else
+            If FormSignIn.Users.ValidateEmail(inputEmail.Text) = True Then
+                MessageBox.Show("Email Sudah Terdaftar !!")
+            Else
+                If inputUsername.Text.Length > 0 Then
+                    If inputEmail.Text.Length > 0 Then
+                        If inputPassword.Text.Length > 0 Then
+                            FormSignIn.Users.AddUsersDatabase(inputUsername.Text, inputEmail.Text, inputPassword.Text)
+                            MessageBox.Show("Sign Up Berhasil !!")
+                            FormSignIn.Show()
+                            Me.Close()
+                        Else
+                            MessageBox.Show("Data Password Belum Terisi !!")
+                        End If
+                    Else
+                        MessageBox.Show("Data Email Belum Terisi !!")
+                    End If
+                Else
+                    MessageBox.Show("Data Username Belum Terisi !!")
+                End If
+            End If
+        End If
     End Sub
+
+
+
+    Private Sub SigninBtn_Click(sender As Object, e As EventArgs) Handles SigninBtn.Click
+        FormSignIn.Show()
+        Me.Close()
+    End Sub
+
+    Private Sub CheckBox1_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox1.CheckedChanged
+        If CheckBox1.Checked = True Then
+            inputPassword.UseSystemPasswordChar = False
+        Else
+            inputPassword.UseSystemPasswordChar = True
+        End If
+    End Sub
+
+    Private Sub FormSignUp_Load(sender As Object, e As EventArgs) Handles Me.Load
+        inputPassword.UseSystemPasswordChar = True
+    End Sub
+
+    'Private Sub FormSignUpAdmin_Closed(sender As Object, e As EventArgs) Handles Me.Closed
+    '    Form1.Show()
+    'End Sub
+
 End Class
