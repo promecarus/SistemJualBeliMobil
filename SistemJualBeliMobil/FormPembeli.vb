a4a14595ac721ecbdf81d9bb24dddb0a77d8b72b
promecarus <haikalslipi@gmail.com>
2022-12-16 23:26:30 +0700

    Add form pembeli starter

--- SistemJualBeliMobil/FormPembeli.vb
+++ SistemJualBeliMobil/FormPembeli.vb
@@ -1,4 +1,15 @@
 ﻿Public Class FormPembeli
+    Public Shared dataPembeli As Pembeli
+    Public Shared selectedRowPembeli
+
+    Public Sub New()
+        ' This call is required by the designer.
+        InitializeComponent()
+
+        ' Add any initialization after the InitializeComponent() call.
+        dataPembeli = New Pembeli
+    End Sub
+
     Private Sub FormPembeli_Closed(sender As Object, e As EventArgs) Handles Me.Closed
         Form1.Show()
     End Sub
