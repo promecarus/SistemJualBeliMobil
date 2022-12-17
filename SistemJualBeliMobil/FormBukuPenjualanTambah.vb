5095d229ec54f09ca6ca63b557c7bee6aab48b9d
promecarus <haikalslipi@gmail.com>
2022-12-17 09:45:41 +0700

    Add constructor

--- SistemJualBeliMobil/FormBukuPenjualanTambah.vb
+++ SistemJualBeliMobil/FormBukuPenjualanTambah.vb
@@ -1,4 +1,21 @@
 ﻿Public Class FormBukuPenjualanTambah
+    Public Sub New()
+        ' This call is required by the designer.
+        InitializeComponent()
+
+        ' Add any initialization after the InitializeComponent() call.
+        ComboBoxIdMobil.DataSource = Form1.Mobil.ListDataMobil
+        ComboBoxIdMobil.DisplayMember = "id"
+        ComboBoxIdMobil.ValueMember = "id"
+
+        ComboBoxIdPembeli.DataSource = Form1.Pembeli.ListDataPembeli
+        ComboBoxIdPembeli.DisplayMember = "nama"
+        ComboBoxIdPembeli.ValueMember = "id_pembeli"
+
+        DateTimePickerTanggalPenjualan.Format = DateTimePickerFormat.Custom
+        DateTimePickerTanggalPenjualan.CustomFormat = "yyyy/MM/dd"
+    End Sub
+
     Private Sub FormBukuPenjualanTambah_Closed(sender As Object, e As EventArgs) Handles Me.Closed
         Form1.Show()
     End Sub
