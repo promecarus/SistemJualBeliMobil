15920b7f71b315163168b4d583cd47436c0a002e
promecarus <haikalslipi@gmail.com>
2022-12-17 10:14:34 +0700

    Update form closed

--- SistemJualBeliMobil/FormBukuPenjualanTambah.vb
+++ SistemJualBeliMobil/FormBukuPenjualanTambah.vb
@@ -30,6 +30,6 @@
     End Sub
 
     Private Sub FormBukuPenjualanTambah_Closed(sender As Object, e As EventArgs) Handles Me.Closed
-        Form1.Show()
+        FormSignIn.Close()
     End Sub
 End Class
