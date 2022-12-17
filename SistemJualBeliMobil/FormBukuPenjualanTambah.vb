e48fd623c0ee27d7b186979904476dd1074c021d
promecarus <haikalslipi@gmail.com>
2022-12-17 16:23:24 +0700

    Fix input cannot be empty

--- SistemJualBeliMobil/FormBukuPenjualanTambah.vb
+++ SistemJualBeliMobil/FormBukuPenjualanTambah.vb
@@ -17,16 +17,20 @@
     End Sub
 
     Private Sub ButtonTambah_Click(sender As Object, e As EventArgs) Handles ButtonTambah.Click
-        FormBukuPenjualan.dataBukuPenjualan.AddDataBukuPenjualanDatabase(
-            ComboBoxIdMobil.SelectedValue,
-            ComboBoxIdPembeli.SelectedValue,
-            TextBoxHargaTerjual.Text,
-            DateTimePickerTanggalPenjualan.Value.ToShortDateString
-        )
+        If Not String.IsNullOrEmpty(TextBoxHargaTerjual.Text) Then
+            FormBukuPenjualan.dataBukuPenjualan.AddDataBukuPenjualanDatabase(
+                ComboBoxIdMobil.SelectedValue,
+                ComboBoxIdPembeli.SelectedValue,
+                TextBoxHargaTerjual.Text,
+                DateTimePickerTanggalPenjualan.Value.ToShortDateString()
+            )
 
-        MessageBox.Show("Data buku penjualan baru berhasil ditambahkan.")
-        Me.Close()
-        FormBukuPenjualan.Show()
+            MessageBox.Show("Data buku penjualan baru berhasil ditambahkan.")
+            Me.Close()
+            FormBukuPenjualan.Show()
+        Else
+            MessageBox.Show("Form tidak boleh kosong!")
+        End If
     End Sub
 
     Private Sub ButtonCancel_Click(sender As Object, e As EventArgs) Handles ButtonCancel.Click
