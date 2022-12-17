ad26b984e57d95660e87449fdb20db1613b884fe
promecarus <haikalslipi@gmail.com>
2022-12-17 10:44:16 +0700

    Add button click no

--- SistemJualBeliMobil/FormBukuPenjualanHapus.vb
+++ SistemJualBeliMobil/FormBukuPenjualanHapus.vb
@@ -1,4 +1,9 @@
 ﻿Public Class FormBukuPenjualanHapus
+    Private Sub ButtonNo_Click(sender As Object, e As EventArgs) Handles ButtonNo.Click
+        Me.Close()
+        FormBukuPenjualan.Show()
+    End Sub
+
     Private Sub FormBukuPenjualanHapus_Closed(sender As Object, e As EventArgs) Handles Me.Closed
         FormSignIn.Close()
     End Sub
