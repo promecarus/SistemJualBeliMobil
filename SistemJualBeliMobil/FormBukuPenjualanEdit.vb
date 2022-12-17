5d34105c77d8e85a962eae43208d33de1db5d849
promecarus <haikalslipi@gmail.com>
2022-12-17 10:41:42 +0700

    Update form closed

--- SistemJualBeliMobil/FormBukuPenjualanEdit.vb
+++ SistemJualBeliMobil/FormBukuPenjualanEdit.vb
@@ -41,6 +41,6 @@
     End Sub
 
     Private Sub FormBukuPenjualanEdit_Closed(sender As Object, e As EventArgs) Handles Me.Closed
-        Form1.Show()
+        FormSignIn.Close()
     End Sub
 End Class
