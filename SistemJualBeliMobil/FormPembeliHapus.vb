73c5eb49901916c889a468caae24598c7e742f9f
promecarus <haikalslipi@gmail.com>
2022-12-17 14:00:35 +0700

    Add constructor

--- SistemJualBeliMobil/FormPembeliHapus.vb
+++ SistemJualBeliMobil/FormPembeliHapus.vb
@@ -1,4 +1,12 @@
 ﻿Public Class FormPembeliHapus
+    Public Sub New()
+        ' This call is required by the designer.
+        InitializeComponent()
+
+        ' Add any initialization after the InitializeComponent() call.
+        LabelNamaPembeli.Text = FormPembeli.dataPembeli.namaProperty
+    End Sub
+
     Private Sub FormPembeliHapus_Closed(sender As Object, e As EventArgs) Handles Me.Closed
         Form1.Show()
     End Sub
