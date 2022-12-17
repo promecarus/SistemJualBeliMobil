eabd2849b6f921b2e2b4c6663002031301e968cd
Sandhi Karunia Sugihartana <sandhi372@gmail.com>
2022-12-17 12:42:31 +0700

    Update BE FormJenisMobilEdit.vb

--- SistemJualBeliMobil/FormJenisMobilEdit.vb
+++ SistemJualBeliMobil/FormJenisMobilEdit.vb
@@ -1,5 +1,31 @@
-﻿Public Class FormJenisMobilEdit
-    Private Sub FormJenisMobilEdit_Closed(sender As Object, e As EventArgs) Handles Me.Closed
-        Form1.Show()
+﻿Imports System.Configuration
+Imports System.Globalization
+Imports System.IO
+
+Public Class FormJenisMobilEdit
+
+    Public Shared JenisMobil As JenisMobil
+
+    Public Sub New()
+
+        ' This call is required by the designer.
+        InitializeComponent()
+
+        ' Add any initialization after the InitializeComponent() call.
+        TxtJenisMobil.Text = FormJenisMobil.JenisMobil.jenisProperty
+
     End Sub
+
+    Private Sub BtnEdit_Click(sender As Object, e As EventArgs) Handles BtnEdit.Click
+        FormJenisMobil.JenisMobil.jenisProperty = TxtJenisMobil.Text.ToString()
+
+        FormJenisMobil.JenisMobil.UpdateDataJenisMobilByIDDatabase(FormJenisMobil.SelectedTableJenisMobil,
+                                                                   FormJenisMobil.JenisMobil.jenisProperty)
+
+        Me.Close()
+    End Sub
+
+    'Private Sub FormJenisMobilEdit_Closed(sender As Object, e As EventArgs) Handles Me.Closed
+    '    Form1.Show()
+    'End Sub
 End Class
