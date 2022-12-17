d5c6b1f069f308af37353151cdf214041068f524
promecarus <haikalslipi@gmail.com>
2022-12-17 14:00:54 +0700

    Add button click yes

--- SistemJualBeliMobil/FormPembeliHapus.vb
+++ SistemJualBeliMobil/FormPembeliHapus.vb
@@ -7,6 +7,13 @@
         LabelNamaPembeli.Text = FormPembeli.dataPembeli.namaProperty
     End Sub
 
+    Private Sub ButtonYes_Click(sender As Object, e As EventArgs) Handles ButtonYes.Click
+        FormPembeli.dataPembeli.DeleteDataPembeliByIDDatabase(FormPembeli.selectedRowPembeli)
+        MessageBox.Show("Data pembeli dengan ID " & FormPembeli.selectedRowPembeli & " berhasil dihapus.")
+        Me.Close()
+        FormPembeli.Show()
+    End Sub
+
     Private Sub FormPembeliHapus_Closed(sender As Object, e As EventArgs) Handles Me.Closed
         Form1.Show()
     End Sub
