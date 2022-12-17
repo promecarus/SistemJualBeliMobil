4713e53510d8e22d8982b2f13f5c9c4ad37c3b38
promecarus <haikalslipi@gmail.com>
2022-12-17 10:40:25 +0700

    Add constructor

--- SistemJualBeliMobil/FormBukuPenjualanEdit.vb
+++ SistemJualBeliMobil/FormBukuPenjualanEdit.vb
@@ -1,4 +1,26 @@
 ﻿Public Class FormBukuPenjualanEdit
+    Public Sub New()
+        ' This call is required by the designer.
+        InitializeComponent()
+
+        ' Add any initialization after the InitializeComponent() call.
+        ComboBoxIdMobil.DataSource = Form1.Mobil.ListDataMobil
+        ComboBoxIdMobil.DisplayMember = "id"
+        ComboBoxIdMobil.ValueMember = "id"
+
+        ComboBoxIdPembeli.DataSource = Form1.Pembeli.ListDataPembeli
+        ComboBoxIdPembeli.DisplayMember = "nama"
+        ComboBoxIdPembeli.ValueMember = "id_pembeli"
+
+        DateTimePickerTanggalPenjualan.Format = DateTimePickerFormat.Custom
+        DateTimePickerTanggalPenjualan.CustomFormat = "yyyy/MM/dd"
+
+        ComboBoxIdMobil.SelectedValue() = FormBukuPenjualan.dataBukuPenjualan.idMobilProperty
+        ComboBoxIdPembeli.SelectedValue() = FormBukuPenjualan.dataBukuPenjualan.idPembeliProperty
+        TextBoxHargaTerjual.Text = FormBukuPenjualan.dataBukuPenjualan.hargaTerjualProperty
+        DateTimePickerTanggalPenjualan.Value = FormBukuPenjualan.dataBukuPenjualan.tanggalPenjualanProperty
+    End Sub
+
     Private Sub FormBukuPenjualanEdit_Closed(sender As Object, e As EventArgs) Handles Me.Closed
         Form1.Show()
     End Sub
