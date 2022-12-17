442da7b6aff5884ea68606206ebbe3b0cd39871a
promecarus <haikalslipi@gmail.com>
2022-12-17 10:50:16 +0700

    Add button click yes

--- SistemJualBeliMobil/FormBukuPenjualanHapus.vb
+++ SistemJualBeliMobil/FormBukuPenjualanHapus.vb
@@ -1,4 +1,11 @@
 ﻿Public Class FormBukuPenjualanHapus
+    Private Sub ButtonYes_Click(sender As Object, e As EventArgs) Handles ButtonYes.Click
+        FormBukuPenjualan.dataBukuPenjualan.DeleteDataBukuPenjualanByIDDatabase(FormBukuPenjualan.selectedRowBukuPenjualan)
+        MessageBox.Show("Data buku penjualan dengan ID " & FormBukuPenjualan.selectedRowBukuPenjualan & " berhasil dihapus.")
+        Me.Close()
+        FormBukuPenjualan.Show()
+    End Sub
+
     Private Sub ButtonNo_Click(sender As Object, e As EventArgs) Handles ButtonNo.Click
         Me.Close()
         FormBukuPenjualan.Show()
