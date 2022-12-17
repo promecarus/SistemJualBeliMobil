4c21bb1142625d53d66b80b871796e65898aac4e
Sandhi Karunia Sugihartana <sandhi372@gmail.com>
2022-12-17 18:34:45 +0700

    Update FormMobilHapus.Designer.vb

--- SistemJualBeliMobil/FormMobilHapus.Designer.vb
+++ SistemJualBeliMobil/FormMobilHapus.Designer.vb
@@ -22,17 +22,59 @@ Partial Class FormMobilHapus
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.BtnHapus = New System.Windows.Forms.Button()
+        Me.LblMobil = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
         Me.SuspendLayout()
+        '
+        'BtnHapus
+        '
+        Me.BtnHapus.Location = New System.Drawing.Point(353, 347)
+        Me.BtnHapus.Name = "BtnHapus"
+        Me.BtnHapus.Size = New System.Drawing.Size(94, 29)
+        Me.BtnHapus.TabIndex = 8
+        Me.BtnHapus.Text = "Hapus"
+        Me.BtnHapus.UseVisualStyleBackColor = True
+        '
+        'LblMobil
+        '
+        Me.LblMobil.Anchor = CType((System.Windows.Forms.AnchorStyles.Left Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.LblMobil.Font = New System.Drawing.Font("Segoe UI", 24.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.LblMobil.ForeColor = System.Drawing.Color.Red
+        Me.LblMobil.Location = New System.Drawing.Point(12, 192)
+        Me.LblMobil.Name = "LblMobil"
+        Me.LblMobil.Size = New System.Drawing.Size(776, 54)
+        Me.LblMobil.TabIndex = 7
+        Me.LblMobil.Text = "ID Mobil"
+        Me.LblMobil.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Label1.Location = New System.Drawing.Point(206, 74)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(389, 28)
+        Me.Label1.TabIndex = 6
+        Me.Label1.Text = "Apakah Ingin Menghapus Mobil Dengan ID"
         '
         'FormMobilHapus
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 20.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.BtnHapus)
+        Me.Controls.Add(Me.LblMobil)
+        Me.Controls.Add(Me.Label1)
         Me.Name = "FormMobilHapus"
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "Hapus Mobil"
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
+
+    Friend WithEvents BtnHapus As Button
+    Friend WithEvents LblMobil As Label
+    Friend WithEvents Label1 As Label
 End Class
