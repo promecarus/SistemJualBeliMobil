1189c41326cb24d824e1a32d419a91e399e98d2a
promecarus <haikalslipi@gmail.com>
2022-12-17 12:59:19 +0700

    Add form activated

--- SistemJualBeliMobil/FormPembeli.vb
+++ SistemJualBeliMobil/FormPembeli.vb
@@ -10,6 +10,15 @@
         dataPembeli = New Pembeli
     End Sub
 
+    Private Sub FormPembeli_Activated(sender As Object, e As EventArgs) Handles Me.Activated
+        DataGridViewPembeli.DataSource = dataPembeli.GetDataPembeliDatabase
+        DataGridViewPembeli.ClearSelection()
+    End Sub
+
+    Private Sub DataGridViewPembeli_ColumnHeaderMouseClick(sender As Object, e As DataGridViewCellMouseEventArgs) Handles DataGridViewPembeli.ColumnHeaderMouseClick
+        DataGridViewPembeli.ClearSelection()
+    End Sub
+
     Private Sub FormPembeli_Closed(sender As Object, e As EventArgs) Handles Me.Closed
         Form1.Show()
     End Sub
