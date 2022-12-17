ea81b26593192a279e77b8900d30d56543b42ac5
promecarus <haikalslipi@gmail.com>
2022-12-17 13:59:56 +0700

    Add button click edit

--- SistemJualBeliMobil/FormPembeliEdit.vb
+++ SistemJualBeliMobil/FormPembeliEdit.vb
@@ -9,6 +9,19 @@
         RichTextBoxAlamat.Text = FormPembeli.dataPembeli.alamatProperty
     End Sub
 
+    Private Sub ButtonEdit_Click(sender As Object, e As EventArgs) Handles ButtonEdit.Click
+        FormPembeli.dataPembeli.UpdateDataPembeliByIDDatabase(
+            FormPembeli.selectedRowPembeli,
+            TextBoxNik.Text,
+            TextBoxNama.Text,
+            RichTextBoxAlamat.Text
+        )
+
+        MessageBox.Show("Data pembeli dengan ID " & FormPembeli.selectedRowPembeli & " berhasil diedit.")
+        Me.Close()
+        FormPembeli.Show()
+    End Sub
+
     Private Sub FormPembeliEdit_Closed(sender As Object, e As EventArgs) Handles Me.Closed
         Form1.Show()
     End Sub
