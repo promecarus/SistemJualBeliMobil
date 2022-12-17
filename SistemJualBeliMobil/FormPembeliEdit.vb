1a9fa1fbfb96296a50c1e30f9d2328cbded6aa51
promecarus <haikalslipi@gmail.com>
2022-12-17 13:59:32 +0700

    Add constructor

--- SistemJualBeliMobil/FormPembeliEdit.vb
+++ SistemJualBeliMobil/FormPembeliEdit.vb
@@ -1,4 +1,14 @@
 ﻿Public Class FormPembeliEdit
+    Public Sub New()
+        ' This call is required by the designer.
+        InitializeComponent()
+
+        ' Add any initialization after the InitializeComponent() call.
+        TextBoxNik.Text = FormPembeli.dataPembeli.nikProperty
+        TextBoxNama.Text = FormPembeli.dataPembeli.namaProperty
+        RichTextBoxAlamat.Text = FormPembeli.dataPembeli.alamatProperty
+    End Sub
+
     Private Sub FormPembeliEdit_Closed(sender As Object, e As EventArgs) Handles Me.Closed
         Form1.Show()
     End Sub
