1fa1134df03c38574ace2601478ad035a3b5b5a0
Dynavx <haical31@gmail.com>
2022-12-18 12:31:28 +0700

    Update FormDashboard.vb

--- SistemJualBeliMobil/FormDashboard.vb
+++ SistemJualBeliMobil/FormDashboard.vb
@@ -2,4 +2,34 @@
     Private Sub FormDashboard_Closed(sender As Object, e As EventArgs) Handles Me.Closed
         Form1.Show()
     End Sub
+
+    Private Sub FormDashboard_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Label2.Parent = PictureBox2
+        Label3.Parent = PictureBox2
+        Label4.Parent = PictureBox2
+    End Sub
+
+
+    Private Sub manageCarBtn_Click(sender As Object, e As EventArgs) Handles manageCarBtn.Click
+        FormMobil.Show()
+        Me.Hide()
+    End Sub
+
+    Private Sub ManageUserBtn_Click(sender As Object, e As EventArgs) Handles ManageUserBtn.Click
+        FormPembeli.Show()
+        Me.Hide()
+    End Sub
+
+    Private Sub TransactionBtn_Click(sender As Object, e As EventArgs) Handles TransactionBtn.Click
+        FormBukuPenjualan.Show()
+        Me.Hide()
+    End Sub
+
+    Private Sub Label2_Click(sender As Object, e As EventArgs) Handles Label2.Click
+
+    End Sub
+
+    Private Sub Label4_Click(sender As Object, e As EventArgs) Handles Label4.Click
+
+    End Sub
 End Class
