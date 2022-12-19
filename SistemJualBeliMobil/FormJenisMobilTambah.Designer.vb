72ad9f8728546931803d850c56002a96e124e25b
Dynavx <haical31@gmail.com>
2022-12-19 13:07:01 +0700

    Update FormJenisMobilTambah.Designer.vb

--- SistemJualBeliMobil/FormJenisMobilTambah.Designer.vb
+++ SistemJualBeliMobil/FormJenisMobilTambah.Designer.vb
@@ -25,13 +25,22 @@ Partial Class FormJenisMobilTambah
         Me.BtnTambah = New System.Windows.Forms.Button()
         Me.Label1 = New System.Windows.Forms.Label()
         Me.TxtJenisMobil = New System.Windows.Forms.TextBox()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'BtnTambah
         '
-        Me.BtnTambah.Location = New System.Drawing.Point(353, 347)
+        Me.BtnTambah.BackgroundImage = Global.SistemJualBeliMobil.My.Resources.Resources.bgbutton
+        Me.BtnTambah.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.BtnTambah.FlatAppearance.BorderSize = 0
+        Me.BtnTambah.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.BtnTambah.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.BtnTambah.ForeColor = System.Drawing.Color.White
+        Me.BtnTambah.Location = New System.Drawing.Point(577, 156)
+        Me.BtnTambah.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
         Me.BtnTambah.Name = "BtnTambah"
-        Me.BtnTambah.Size = New System.Drawing.Size(94, 29)
+        Me.BtnTambah.Size = New System.Drawing.Size(82, 28)
         Me.BtnTambah.TabIndex = 8
         Me.BtnTambah.Text = "Tambah"
         Me.BtnTambah.UseVisualStyleBackColor = True
@@ -39,31 +48,47 @@ Partial Class FormJenisMobilTambah
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
-        Me.Label1.Location = New System.Drawing.Point(309, 74)
+        Me.Label1.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Label1.Location = New System.Drawing.Point(388, 80)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(183, 28)
+        Me.Label1.Size = New System.Drawing.Size(162, 21)
         Me.Label1.TabIndex = 6
         Me.Label1.Text = "Tambah Jenis Mobil"
         '
         'TxtJenisMobil
         '
-        Me.TxtJenisMobil.Location = New System.Drawing.Point(272, 212)
+        Me.TxtJenisMobil.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.TxtJenisMobil.Location = New System.Drawing.Point(388, 114)
+        Me.TxtJenisMobil.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
         Me.TxtJenisMobil.Name = "TxtJenisMobil"
-        Me.TxtJenisMobil.Size = New System.Drawing.Size(249, 27)
+        Me.TxtJenisMobil.Size = New System.Drawing.Size(271, 29)
         Me.TxtJenisMobil.TabIndex = 9
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BackgroundImage = Global.SistemJualBeliMobil.My.Resources.Resources.MobilBawah
+        Me.PictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom
+        Me.PictureBox1.Location = New System.Drawing.Point(-148, -5)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(633, 351)
+        Me.PictureBox1.TabIndex = 14
+        Me.PictureBox1.TabStop = False
         '
         'FormJenisMobilTambah
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 20.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.BackColor = System.Drawing.Color.White
+        Me.ClientSize = New System.Drawing.Size(700, 338)
         Me.Controls.Add(Me.TxtJenisMobil)
         Me.Controls.Add(Me.BtnTambah)
         Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
         Me.Name = "FormJenisMobilTambah"
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "Tambah Jenis Mobil"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -72,4 +97,5 @@ Partial Class FormJenisMobilTambah
     Friend WithEvents BtnTambah As Button
     Friend WithEvents Label1 As Label
     Friend WithEvents TxtJenisMobil As TextBox
+    Friend WithEvents PictureBox1 As PictureBox
 End Class
