a8e99305a1566fc21526fc0badbc74dcfb2f4cca
Dynavx <haical31@gmail.com>
2022-12-19 13:06:50 +0700

    Update FormDashboard.vb

--- SistemJualBeliMobil/FormDashboard.vb
+++ SistemJualBeliMobil/FormDashboard.vb
@@ -32,4 +32,24 @@
     Private Sub Label4_Click(sender As Object, e As EventArgs) Handles Label4.Click
 
     End Sub
+
+    Private Sub PictureBox2_Click(sender As Object, e As EventArgs) Handles PictureBox2.Click
+
+    End Sub
+
+    Private Sub BtnIndex_Click(sender As Object, e As EventArgs) Handles BtnIndex.Click
+
+    End Sub
+
+    Private Sub Label5_Click(sender As Object, e As EventArgs) Handles Label5.Click
+
+    End Sub
+
+    Private Sub PictureBox1_Click(sender As Object, e As EventArgs) Handles PictureBox1.Click
+
+    End Sub
+
+    Private Sub SignoutBtn_Click(sender As Object, e As EventArgs) Handles SignoutBtn.Click
+
+    End Sub
 End Class
