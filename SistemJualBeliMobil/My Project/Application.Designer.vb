1b7b7a4dd1f47ce3703691ba22d890a2919b3d90
promecarus <haikalslipi@gmail.com>
2022-12-20 21:16:22 +0700

    Change start to form sign in

--- SistemJualBeliMobil/My Project/Application.Designer.vb
+++ SistemJualBeliMobil/My Project/Application.Designer.vb
@@ -31,7 +31,7 @@ Namespace My
 
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Form1
+            Me.MainForm = Global.SistemJualBeliMobil.FormSignIn
         End Sub
     End Class
 End Namespace
