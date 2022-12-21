cb8cb6825dfb02e416cbfcea591c1e706963bd68
Dynavx <haical31@gmail.com>
2022-12-21 08:19:22 +0700

    Update FormBukuPenjualan.vb

--- SistemJualBeliMobil/FormBukuPenjualan.vb
+++ SistemJualBeliMobil/FormBukuPenjualan.vb
@@ -30,42 +30,6 @@
         End If
     End Sub
 
-    Private Sub ButtonTambah_Click(sender As Object, e As EventArgs)
-        FormBukuPenjualanTambah.Show()
-        Me.Hide()
-    End Sub
-
-    Private Sub ButtonEdit_Click(sender As Object, e As EventArgs)
-        Try
-            Dim selectedBukuPenjualan As List(Of String) = dataBukuPenjualan.GetDataBukuPenjualanByIDDatabase(selectedRowBukuPenjualan)
-
-            dataBukuPenjualan.idMobilProperty = selectedBukuPenjualan(1)
-            dataBukuPenjualan.idPembeliProperty = selectedBukuPenjualan(2)
-            dataBukuPenjualan.hargaTerjualProperty = selectedBukuPenjualan(3)
-            dataBukuPenjualan.tanggalPenjualanProperty = selectedBukuPenjualan(4)
-
-            Dim formEdit = New FormBukuPenjualanEdit()
-            formEdit.Show()
-            Me.Hide()
-        Catch ex As Exception
-            MessageBox.Show("Pilih row terlebih dahulu!")
-        End Try
-    End Sub
-
-    Private Sub ButtonHapus_Click(sender As Object, e As EventArgs)
-        Try
-            Dim selectedBukuPenjualan As List(Of String) = dataBukuPenjualan.GetDataBukuPenjualanByIDDatabase(selectedRowBukuPenjualan)
-
-            dataBukuPenjualan.idMobilProperty = selectedBukuPenjualan(1)
-
-            Dim formHapus = New FormBukuPenjualanHapus()
-            formHapus.Show()
-            Me.Hide()
-        Catch ex As Exception
-            MessageBox.Show("Pilih row terlebih dahulu!")
-        End Try
-    End Sub
-
     Private Sub FormBukuPenjualan_Closed(sender As Object, e As EventArgs) Handles Me.Closed
         FormSignIn.Close()
     End Sub
@@ -86,6 +50,42 @@
 
     Private Sub BtnIndex_Click(sender As Object, e As EventArgs) Handles BtnIndex.Click
         FormDashboard.Show()
+        Me.Hide()
+    End Sub
+
+    Private Sub ButtonEdit_Click(sender As Object, e As EventArgs) Handles ButtonEdit.Click
+        Try
+            Dim selectedBukuPenjualan As List(Of String) = dataBukuPenjualan.GetDataBukuPenjualanByIDDatabase(selectedRowBukuPenjualan)
+
+            dataBukuPenjualan.idMobilProperty = selectedBukuPenjualan(1)
+            dataBukuPenjualan.idPembeliProperty = selectedBukuPenjualan(2)
+            dataBukuPenjualan.hargaTerjualProperty = selectedBukuPenjualan(3)
+            dataBukuPenjualan.tanggalPenjualanProperty = selectedBukuPenjualan(4)
+
+            Dim formEdit = New FormBukuPenjualanEdit()
+            formEdit.Show()
+            Me.Hide()
+        Catch ex As Exception
+            MessageBox.Show("Pilih row terlebih dahulu!")
+        End Try
+    End Sub
+
+    Private Sub ButtonHapus_Click(sender As Object, e As EventArgs) Handles ButtonHapus.Click
+        Try
+            Dim selectedBukuPenjualan As List(Of String) = dataBukuPenjualan.GetDataBukuPenjualanByIDDatabase(selectedRowBukuPenjualan)
+
+            dataBukuPenjualan.idMobilProperty = selectedBukuPenjualan(1)
+
+            Dim formHapus = New FormBukuPenjualanHapus()
+            formHapus.Show()
+            Me.Hide()
+        Catch ex As Exception
+            MessageBox.Show("Pilih row terlebih dahulu!")
+        End Try
+    End Sub
+
+    Private Sub ButtonTambah_Click(sender As Object, e As EventArgs) Handles ButtonTambah.Click
+        FormBukuPenjualanTambah.Show()
         Me.Hide()
     End Sub
 End Class
