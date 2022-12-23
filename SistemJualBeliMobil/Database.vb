214f8e0997318faca38b31b05b85d419a9a91186
promecarus <haikalslipi@gmail.com>
2022-12-24 01:04:42 +0700

    Create Database.vb

---
+++ SistemJualBeliMobil/Database.vb
@@ -0,0 +1,15 @@
+﻿Imports MySql.Data.MySqlClient
+
+Public Class Database
+    Private server As String = "localhost"
+    Private username As String = "root"
+    Private password As String = ""
+    Private database As String = "db_sistem_jual_beli_mobil"
+
+    Private connection As New MySqlConnection("server=" + server + ";" + "user id=" + username + ";" + "password=" + password + ";" + "database=" + database)
+    Private command As New MySqlCommand
+
+    Public Sub New()
+        command.Connection = connection
+    End Sub
+End Class
