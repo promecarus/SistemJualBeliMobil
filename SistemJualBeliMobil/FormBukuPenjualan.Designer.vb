b52da3e38b8527b4b4950322b366830506faa77f
Sandhi Karunia Sugihartana <sandhi372@gmail.com>
2022-12-23 16:10:26 +0700

    Update FormBukuPenjualan.Designer.vb

--- SistemJualBeliMobil/FormBukuPenjualan.Designer.vb
+++ SistemJualBeliMobil/FormBukuPenjualan.Designer.vb
@@ -174,7 +174,6 @@ Partial Class FormBukuPenjualan
         Me.ButtonEdit.FlatStyle = System.Windows.Forms.FlatStyle.Flat
         Me.ButtonEdit.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
         Me.ButtonEdit.ForeColor = System.Drawing.Color.White
-        Me.ButtonEdit.Location = New System.Drawing.Point(620, 87)
         Me.ButtonEdit.Location = New System.Drawing.Point(623, 87)
         Me.ButtonEdit.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
         Me.ButtonEdit.Name = "ButtonEdit"
