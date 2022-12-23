97576ed8e97b575136c529f9a2f7af55011880ea
mawanrunner <109322835+mawanrunner@users.noreply.github.com>
2022-12-23 20:13:31 +0700

    add sign out method

--- SistemJualBeliMobil/FormMobil.vb
+++ SistemJualBeliMobil/FormMobil.vb
@@ -160,4 +160,9 @@
             BtnSearch.Enabled = False
         End If
     End Sub
+
+    Private Sub SignoutBtn_Click(sender As Object, e As EventArgs) Handles SignoutBtn.Click
+        FormSignIn.Show()
+        Me.Hide()
+    End Sub
 End Class
