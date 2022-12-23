52a338ce51156bbe2643d54636616647f765df06
promecarus <haikalslipi@gmail.com>
2022-12-24 01:04:53 +0700

    Update Database.vb

--- SistemJualBeliMobil/Database.vb
+++ SistemJualBeliMobil/Database.vb
@@ -12,4 +12,34 @@ Public Class Database
     Public Sub New()
         command.Connection = connection
     End Sub
+
+    Public Function ExecuteQuery(query As String) As DataTable
+        Dim dataTable As New DataTable()
+
+        Try
+            connection.Open()
+            command.CommandText = query
+            dataTable.Load(command.ExecuteReader())
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        Finally
+            connection.Close()
+            connection.Dispose()
+        End Try
+
+        Return dataTable
+    End Function
+
+    Public Sub ExecuteNonQuery(query As String)
+        Try
+            connection.Open()
+            command.CommandText = query
+            command.ExecuteNonQuery()
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        Finally
+            connection.Close()
+            connection.Dispose()
+        End Try
+    End Sub
 End Class
