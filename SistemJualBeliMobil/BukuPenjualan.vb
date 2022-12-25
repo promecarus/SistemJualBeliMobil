021b88b42a7dc5d639fdf623e795f07a554a6b30
promecarus <haikalslipi@gmail.com>
2022-12-25 15:00:12 +0700

    Revert "Update add"
    This reverts commit 737b33d8474d813eadf9ac2dba7edea3eb19b756.

--- SistemJualBeliMobil/BukuPenjualan.vb
+++ SistemJualBeliMobil/BukuPenjualan.vb
@@ -40,15 +40,37 @@
         End Set
     End Property
 
-    Public Sub AddDataBukuPenjualanDatabase(
-        id_mobil As Integer,
-        id_pembeli As Integer,
-        harga_terjual As Integer,
-        tanggal_penjualan As Date
-     )
-        Dim query = "INSERT INTO BUKU_PENJUALAN(id_mobil, id_pembeli, harga_terjual, tanggal_penjualan) VALUES('" & id_mobil & "', '" & id_pembeli & "', '" & harga_terjual & "', '" & tanggal_penjualan.ToString("yyyy/MM/dd") & "')"
-        FormSignIn.db.ExecuteNonQuery(query)
-    End Sub
+    Public Function AddDataBukuPenjualanDatabase(id_mobil As Integer,
+                                           id_pembeli As Integer,
+                                           harga_terjual As Integer,
+                                           tanggal_penjualan As Date)
+
+        dbConn.ConnectionString = "server =" + server + ";" + "user id=" + username_db + ";" _
+                + "password=" + password_db + ";" + "database =" + database
+
+        Try
+            dbConn.Open()
+
+            sqlCommand.Connection = dbConn
+            sqlQuery = "INSERT INTO BUKU_PENJUALAN(id_mobil, id_pembeli, harga_terjual, tanggal_penjualan) VALUES('" _
+                        & id_mobil & "', '" _
+                        & id_pembeli & "', '" _
+                        & harga_terjual & "', '" _
+                        & tanggal_penjualan.ToString("yyyy/MM/dd") & "')"
+
+            sqlCommand = New MySqlCommand(sqlQuery, dbConn)
+            sqlRead = sqlCommand.ExecuteReader
+
+            dbConn.Close()
+
+            sqlRead.Close()
+            dbConn.Close()
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        Finally
+            dbConn.Dispose()
+        End Try
+    End Function
 
     Public Function GetDataBukuPenjualanDatabase() As DataTable
         Dim query = "SELECT id_penjualan AS 'ID', id_mobil AS 'ID Mobil', id_pembeli AS 'ID Pembeli', harga_terjual AS 'Harga Terjual', tanggal_penjualan AS 'Tanggal Penjualan' FROM buku_penjualan"
