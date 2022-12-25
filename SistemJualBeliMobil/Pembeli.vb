4c963ac2c233ccb103dd556c967950148d20b7c9
promecarus <haikalslipi@gmail.com>
2022-12-25 14:17:10 +0700

    Update list

--- SistemJualBeliMobil/Pembeli.vb
+++ SistemJualBeliMobil/Pembeli.vb
@@ -151,29 +151,7 @@ Public Class Pembeli
     End Function
 
     Public Function ListDataPembeli() As DataTable
-        dbConn.ConnectionString = "server =" + server + ";" + "user id=" + username_db + ";" _
-                + "password=" + password_db + ";" + "database =" + database
-
-        Try
-            dbConn.Open()
-
-            sqlCommand.Connection = dbConn
-            sqlQuery = "SELECT id_pembeli, nama FROM pembeli"
-
-            Debug.WriteLine(sqlQuery)
-
-            Dim adapter As New MySqlDataAdapter(sqlQuery, dbConn)
-            Dim table As New DataTable()
-
-            adapter.Fill(table)
-
-            dbConn.Close()
-
-            Return table
-        Catch ex As Exception
-            MessageBox.Show(ex.Message)
-        Finally
-            dbConn.Dispose()
-        End Try
+        Dim query = "SELECT id_pembeli, nama FROM pembeli"
+        Return FormSignIn.db.ExecuteQuery(query)
     End Function
 End Class
