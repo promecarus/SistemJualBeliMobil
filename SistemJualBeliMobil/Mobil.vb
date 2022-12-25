64df595d3a016dc606c4af55b9764069003c719e
promecarus <haikalslipi@gmail.com>
2022-12-25 15:54:40 +0700

    Update search none

--- SistemJualBeliMobil/Mobil.vb
+++ SistemJualBeliMobil/Mobil.vb
@@ -123,33 +123,8 @@ Public Class Mobil
     End Function
 
     Public Function GetDataMobilDatabaseSearchNone(ID As Integer) As DataTable
-        Dim result As New DataTable
-
-        dbConn.ConnectionString = "server =" + server + ";" + "user id=" + username_db + ";" _
-                + "password=" + password_db + ";" + "database =" + database
-
-        dbConn.Open()
-
-        sqlCommand.Connection = dbConn
-        sqlCommand.CommandText = "SELECT id AS 'ID',
-                                  id_jenis_mobil AS 'ID Jenis Mobil',
-                                  tipe_mobil AS 'Tipe Mobil',
-                                  tahun_pembuatan AS 'Tahun Pembuatan',
-                                  kondisi AS 'Kondisi',
-                                  harga AS 'Harga',
-                                  garansi AS 'Garansi',
-                                  status_terjual AS 'Status Terjual',
-                                  harga_default AS 'Harga Default'
-                                  FROM MOBIL WHERE id='" & ID & "'"
-
-        sqlRead = sqlCommand.ExecuteReader
-
-        result.Load(sqlRead)
-
-        sqlRead.Close()
-        dbConn.Close()
-
-        Return result
+        Dim query = "SELECT id AS 'ID', id_jenis_mobil AS 'ID Jenis Mobil', tipe_mobil AS 'Tipe Mobil', tahun_pembuatan AS 'Tahun Pembuatan', kondisi AS 'Kondisi', harga AS 'Harga', garansi AS 'Garansi', status_terjual AS 'Status Terjual', harga_default AS 'Harga Default' FROM mobil WHERE id='" & ID & "'"
+        Return FormSignIn.db.ExecuteQuery(query)
     End Function
 
     Public Function GetDataMobilDatabaseSearchTerjual(ID As Integer) As DataTable
