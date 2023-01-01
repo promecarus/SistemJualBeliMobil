df7899269f3db452cc72519ea14fef5be15a38e6
promecarus <haikalslipi@gmail.com>
2023-01-01 15:17:55 +0700

    Remove constructor form pembeli

--- SistemJualBeliMobil/FormPembeli.vb
+++ SistemJualBeliMobil/FormPembeli.vb
@@ -1,14 +1,6 @@
 ﻿Public Class FormPembeli
-    Public Shared dataPembeli As Pembeli
+    Public Shared dataPembeli As New Pembeli
     Public Shared selectedRowPembeli
-
-    Public Sub New()
-        ' This call is required by the designer.
-        InitializeComponent()
-
-        ' Add any initialization after the InitializeComponent() call.
-        dataPembeli = New Pembeli
-    End Sub
 
     Private Sub FormPembeli_Activated(sender As Object, e As EventArgs) Handles Me.Activated
         DataGridViewPembeli.DataSource = dataPembeli.Read
