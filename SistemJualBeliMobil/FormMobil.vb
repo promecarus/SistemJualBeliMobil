142b111c82a60f381eddf78ea19fe0b177d2a237
mawanrunner <109322835+mawanrunner@users.noreply.github.com>
2023-01-04 09:34:01 +0700

    Update FormMobil.vb

--- SistemJualBeliMobil/FormMobil.vb
+++ SistemJualBeliMobil/FormMobil.vb
@@ -86,7 +86,7 @@
 
             FormMobilEdit.Show()
         Catch ex As Exception
-            MessageBox.Show("Pilih Row Terlebih Dahulu !!")
+            MessageBox.Show("Pilih row terlebih dahulu !!")
         End Try
     End Sub
 
