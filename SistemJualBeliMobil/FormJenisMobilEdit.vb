8197510ac0790afbce524798631c148c2621459f
mawanrunner <109322835+mawanrunner@users.noreply.github.com>
2023-01-04 09:30:06 +0700

    Update FormJenisMobilEdit.vb

--- SistemJualBeliMobil/FormJenisMobilEdit.vb
+++ SistemJualBeliMobil/FormJenisMobilEdit.vb
@@ -5,8 +5,8 @@
 
     Private Sub ButtonEdit_Click(sender As Object, e As EventArgs) Handles BtnEdit.Click
         If TxtJenisMobil.Text = "" Then
-            MessageBox.Show("Data jenis Mobil Tidak boleh kosong!")
-            Elme
+            MessageBox.Show("Data jenis mobil tidak boleh kosong!")
+        Else
             FormJenisMobil.JenisMobil.jenisProperty = TxtJenisMobil.Text.ToString()
 
             FormJenisMobil.JenisMobil.Update(
@@ -14,7 +14,7 @@
                 FormJenisMobil.JenisMobil.jenisProperty
             )
 
-            MessageBox.Show("Data jenis mobil dengan ID " & FormJenisMobil.SelectedRowJenisMobil & " Berhasil diedit.")
+            MessageBox.Show("Data Jenis Mobil dengan ID " & FormJenisMobil.SelectedRowJenisMobil & " Berhasil Diedit.")
 
             Me.Hide()
         End If
